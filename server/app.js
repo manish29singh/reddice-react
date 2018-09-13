6e029127d95be5a2581d2308e5378f9381d03e38
Manish Singh <manish.singh.9277@gmail.com>
2018-09-13 18:16:48 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import path from "path";
+
+let app = express();
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.listen(8000, () => {
+  console.log("Server started at port 8000");
+});
