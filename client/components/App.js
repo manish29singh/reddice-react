c7675dea12520fb3ddef8c768202dd98aa61833d
Manish Singh <manish.singh.9277@gmail.com>
2018-09-13 20:30:35 +0530

    small break

--- client/components/App.js
+++ client/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default () => {
-  return <h1>Hello from react </h1>;
-};
+class App extends React.Component {
+  render() {
+    return <h1>Hello from react</h1>;
+  }
+}
+
+export default App;
